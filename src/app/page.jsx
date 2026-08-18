c23567cf96913e4eaaa888e55adb0058b5c4cba4
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-19 00:34:37 +0200

    feat: connect projects to authenticated Supabase user

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,13 @@
-'use client';
+﻿'use client';
 
 import React from 'react';
 import App from '../../App';
 import AuthGate from '../components/AuthGate';
-import RealUserDataBridge from '../components/RealUserDataBridge';
 
 export default function HomePage() {
   return (
     <AuthGate>
-      <RealUserDataBridge>
-        <App />
-      </RealUserDataBridge>
+      <App />
     </AuthGate>
   );
 }
