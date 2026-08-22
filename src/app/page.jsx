02f81eda3da6a9653319104fb3bc4f1a95dd0f94
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-22 02:32:12 +0200

    fix: open original projects workspace from routed entry

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 import App from '../../App';
 import AuthGate from '../components/AuthGate';
 import ProjectDeleteEnhancer from '../components/ProjectDeleteEnhancer';
+import LegacyProjectsRouteBridge from '../components/LegacyProjectsRouteBridge';
 
 export default function HomePage() {
   return (
     <AuthGate>
       <ProjectDeleteEnhancer />
+      <LegacyProjectsRouteBridge />
       <App />
     </AuthGate>
   );
