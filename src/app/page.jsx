ab6cf32cf3234e325571352bb95c87ff86eb29c4
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-22 17:45:09 +0200

    feat(ui): mount admin user and user experience enhancers

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,16 @@ import App from '../../App';
 import AuthGate from '../components/AuthGate';
 import ProjectDeleteEnhancer from '../components/ProjectDeleteEnhancer';
 import LegacyProjectsRouteBridge from '../components/LegacyProjectsRouteBridge';
+import AdminUserManagementEnhancer from '../components/AdminUserManagementEnhancer';
+import UserExperienceEnhancer from '../components/UserExperienceEnhancer';
 
 export default function HomePage() {
   return (
     <AuthGate>
       <ProjectDeleteEnhancer />
       <LegacyProjectsRouteBridge />
+      <AdminUserManagementEnhancer />
+      <UserExperienceEnhancer />
       <App />
     </AuthGate>
   );
