c3978961e2983acca4e57d6af10b9e528b523b5f
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:05:33 +0200

    Add admin operations department screen

---
+++ src/app/admin/operations/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import AdminDepartmentDashboard from '../../../components/AdminDepartmentDashboard';
+export default function Page(){return <WorkspaceDashboardShell admin title="التشغيل والتحليلات" subtitle="مؤشرات الأداء والحالة التشغيلية"><AdminDepartmentDashboard type="operations"/></WorkspaceDashboardShell>;}
