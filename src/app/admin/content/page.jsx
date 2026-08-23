f4b939e2be0d833c8df53c312add7cd4cbc17ced
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:05:29 +0200

    Add admin content department screen

---
+++ src/app/admin/content/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import AdminDepartmentDashboard from '../../../components/AdminDepartmentDashboard';
+export default function Page(){return <WorkspaceDashboardShell admin title="المحتوى والتسويق" subtitle="إدارة محتوى المنصة والحملات"><AdminDepartmentDashboard type="content"/></WorkspaceDashboardShell>;}
