4b5dd8eb91c8d7b2eb42aa3351138f736cbcd928
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:05:00 +0200

    Add commerce dashboard

---
+++ src/app/dashboard/commerce/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import DashboardSpecialtyPage from '../../../components/DashboardSpecialtyPage';
+export default function Page(){return <WorkspaceDashboardShell title="المتجر والمشتريات" subtitle="المنتجات والخدمات والرصيد"><DashboardSpecialtyPage type="commerce"/></WorkspaceDashboardShell>;}
