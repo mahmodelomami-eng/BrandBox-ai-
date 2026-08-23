6f6ed29d5e2ff1187cd1a8b8b3b6f48c4a3f2871
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 03:22:05 +0200

    fix: wrap root search params router in suspense

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import App from '../../App';
 import HomeExperience from '../components/HomeExperience';
@@ -12,7 +12,7 @@ import UserExperienceEnhancer from '../components/UserExperienceEnhancer';
 import SidebarPricingEnhancer from '../components/SidebarPricingEnhancer';
 import PricingRouteIntentEnhancer from '../components/PricingRouteIntentEnhancer';
 
-export default function HomePage() {
+function RootExperience() {
   const searchParams = useSearchParams();
   const legacyView = searchParams.get('view');
 
@@ -29,4 +29,8 @@ export default function HomePage() {
       <App />
     </AuthGate>
   );
+}
+
+export default function HomePage() {
+  return <Suspense fallback={<div className="min-h-screen bg-[#050506]" />}><RootExperience /></Suspense>;
 }
