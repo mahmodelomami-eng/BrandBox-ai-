a953611a54b43ca1f5fc41d041074bc3d10265a7
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:05:08 +0200

    Add dedicated admin control center

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../components/WorkspaceDashboardShell';
+import AdminDepartmentDashboard from '../../components/AdminDepartmentDashboard';
+export default function Page(){return <WorkspaceDashboardShell admin title="مركز إدارة المنصة" subtitle="الإدارات والصلاحيات والتشغيل"><AdminDepartmentDashboard type="overview"/></WorkspaceDashboardShell>;}
