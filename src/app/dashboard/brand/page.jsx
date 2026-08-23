10984d0641757d30b87ae1de899c596e280382d4
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:04:44 +0200

    Add brand specialty dashboard

---
+++ src/app/dashboard/brand/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import DashboardSpecialtyPage from '../../../components/DashboardSpecialtyPage';
+export default function Page(){return <WorkspaceDashboardShell title="الهوية والعلامة التجارية" subtitle="إدارة الهوية والقوالب والأصول"><DashboardSpecialtyPage type="brand"/></WorkspaceDashboardShell>;}
