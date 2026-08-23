7d39898eee1e6dd55b21ce0d7684a2fa8953ce30
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 03:19:43 +0200

    fix: preserve legacy authenticated workspace views

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,32 @@
 'use client';
 
 import React from 'react';
+import { useSearchParams } from 'next/navigation';
+import App from '../../App';
 import HomeExperience from '../components/HomeExperience';
+import AuthGate from '../components/AuthGate';
+import ProjectDeleteEnhancer from '../components/ProjectDeleteEnhancer';
+import LegacyProjectsRouteBridge from '../components/LegacyProjectsRouteBridge';
+import AdminUserManagementEnhancer from '../components/AdminUserManagementEnhancer';
+import UserExperienceEnhancer from '../components/UserExperienceEnhancer';
+import SidebarPricingEnhancer from '../components/SidebarPricingEnhancer';
+import PricingRouteIntentEnhancer from '../components/PricingRouteIntentEnhancer';
 
 export default function HomePage() {
-  return <HomeExperience />;
+  const searchParams = useSearchParams();
+  const legacyView = searchParams.get('view');
+
+  if (!legacyView) return <HomeExperience />;
+
+  return (
+    <AuthGate>
+      <ProjectDeleteEnhancer />
+      <LegacyProjectsRouteBridge />
+      <AdminUserManagementEnhancer />
+      <UserExperienceEnhancer />
+      <SidebarPricingEnhancer />
+      <PricingRouteIntentEnhancer />
+      <App />
+    </AuthGate>
+  );
 }
