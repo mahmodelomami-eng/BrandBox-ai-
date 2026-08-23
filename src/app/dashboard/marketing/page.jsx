12d0995b0f68a332ddc848b26fa3fadd233b4893
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:04:50 +0200

    Add marketing specialty dashboard

---
+++ src/app/dashboard/marketing/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import DashboardSpecialtyPage from '../../../components/DashboardSpecialtyPage';
+export default function Page(){return <WorkspaceDashboardShell title="التسويق والمحتوى" subtitle="التخطيط والكتابة والتصميم للحملات"><DashboardSpecialtyPage type="marketing"/></WorkspaceDashboardShell>;}
