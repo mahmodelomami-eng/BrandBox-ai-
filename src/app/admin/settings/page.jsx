79a7339a5d33fc99e3e57dd0ec751c63505f4d00
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:05:38 +0200

    Add admin settings department screen

---
+++ src/app/admin/settings/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import AdminDepartmentDashboard from '../../../components/AdminDepartmentDashboard';
+export default function Page(){return <WorkspaceDashboardShell admin title="إعدادات المنصة" subtitle="التكوين العام والتكاملات والسياسات"><AdminDepartmentDashboard type="settings"/></WorkspaceDashboardShell>;}
