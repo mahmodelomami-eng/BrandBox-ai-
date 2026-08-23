742bbabe86e6f9e11f5ad7ae702e0cbce57c0adc
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:04:39 +0200

    Add AI studio dashboard

---
+++ src/app/dashboard/studio/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import DashboardSpecialtyPage from '../../../components/DashboardSpecialtyPage';
+export default function Page(){return <WorkspaceDashboardShell title="استوديو الذكاء الاصطناعي" subtitle="الأدوات الإبداعية الأساسية"><DashboardSpecialtyPage type="studio"/></WorkspaceDashboardShell>;}
