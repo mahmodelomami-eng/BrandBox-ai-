a6dc5f83b8a368b92fd08842cd04483f17a85623
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 16:29:31 +0200

    fix: make video workspace route server-render safe

--- src/app/projects/video/workspace/page.jsx
+++ src/app/projects/video/workspace/page.jsx
@@ -1,11 +1,7 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
 import AuthGate from '../../../../components/AuthGate';
 import MediaProjectWorkspace from '../../../../components/MediaProjectWorkspace';
 
-export default function VideoProjectPage() {
-  const searchParams = useSearchParams();
-  const projectId = searchParams.get('project') || '';
+export default function VideoProjectPage({ searchParams }) {
+  const projectId = searchParams?.project || '';
   return <AuthGate><MediaProjectWorkspace tool="video" projectId={projectId} /></AuthGate>;
 }
