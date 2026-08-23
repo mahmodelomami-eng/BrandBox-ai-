9e7df60e871fa09b3f5d89da83f9ac30e7c74eaa
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-24 01:56:09 +0200

    refactor: redirect finance admin route to live control center

--- src/app/admin/finance/page.jsx
+++ src/app/admin/finance/page.jsx
@@ -1,4 +1,5 @@
-'use client';
-import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
-import AdminDepartmentDashboard from '../../../components/AdminDepartmentDashboard';
-export default function Page(){return <WorkspaceDashboardShell admin title="المالية والاشتراكات" subtitle="الباقات والأرصدة والمدفوعات"><AdminDepartmentDashboard type="finance"/></WorkspaceDashboardShell>;}
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+  redirect('/admin?section=finance');
+}
