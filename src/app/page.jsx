598a2ae2c7d29d502b31cef3acb5841a4035711e
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 16:56:48 +0200

    fix: apply support admin route guard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,6 +14,7 @@ import PricingRouteIntentEnhancer from '../components/PricingRouteIntentEnhancer
 import ImageStudioWorkspace from '../components/ImageStudioWorkspace';
 import ProjectsToolHub from '../components/ProjectsToolHub';
 import UserDashboardToolChooserEnhancer from '../components/UserDashboardToolChooserEnhancer';
+import AdminRoleRouteGuardEnhancer from '../components/AdminRoleRouteGuardEnhancer';
 
 function RootExperience() {
   const searchParams = useSearchParams();
@@ -42,6 +43,7 @@ function RootExperience() {
       <LegacyWorkspaceStateBridge view={legacyView}>
         <ProjectDeleteEnhancer />
         <AdminUserManagementEnhancer />
+        <AdminRoleRouteGuardEnhancer />
         <UserExperienceEnhancer />
         <SidebarPricingEnhancer />
         <PricingRouteIntentEnhancer />
