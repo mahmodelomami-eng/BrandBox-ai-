4f046cf6b6bd42796b2588b24fa6dfd8f6abb805
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:05:04 +0200

    Add account dashboard

---
+++ src/app/dashboard/account/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import DashboardSpecialtyPage from '../../../components/DashboardSpecialtyPage';
+export default function Page(){return <WorkspaceDashboardShell title="الحساب والرصيد" subtitle="بيانات الحساب، الرصيد والمشتريات"><DashboardSpecialtyPage type="account"/></WorkspaceDashboardShell>;}
