757456746a8edddd675a0693128a283218082ce9
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 03:19:31 +0200

    feat: make redesigned experience the root home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,8 @@
-﻿'use client';
+'use client';
 
 import React from 'react';
-import App from '../../App';
-import AuthGate from '../components/AuthGate';
-import ProjectDeleteEnhancer from '../components/ProjectDeleteEnhancer';
-import LegacyProjectsRouteBridge from '../components/LegacyProjectsRouteBridge';
-import AdminUserManagementEnhancer from '../components/AdminUserManagementEnhancer';
-import UserExperienceEnhancer from '../components/UserExperienceEnhancer';
-import SidebarPricingEnhancer from '../components/SidebarPricingEnhancer';
-import PricingRouteIntentEnhancer from '../components/PricingRouteIntentEnhancer';
+import HomeExperience from '../components/HomeExperience';
 
 export default function HomePage() {
-  return (
-    <AuthGate>
-      <ProjectDeleteEnhancer />
-      <LegacyProjectsRouteBridge />
-      <AdminUserManagementEnhancer />
-      <UserExperienceEnhancer />
-      <SidebarPricingEnhancer />
-      <PricingRouteIntentEnhancer />
-      <App />
-    </AuthGate>
-  );
+  return <HomeExperience />;
 }
