9ca6df778295e03c510a8b94b24e5d1cc10505e5
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-08-23 23:04:55 +0200

    Add print specialty dashboard

---
+++ src/app/dashboard/print/page.jsx
@@ -0,0 +1,4 @@
+'use client';
+import WorkspaceDashboardShell from '../../../components/WorkspaceDashboardShell';
+import DashboardSpecialtyPage from '../../../components/DashboardSpecialtyPage';
+export default function Page(){return <WorkspaceDashboardShell title="الطباعة والإنتاج" subtitle="من التصميم إلى التجهيز والتنفيذ"><DashboardSpecialtyPage type="print"/></WorkspaceDashboardShell>;}
