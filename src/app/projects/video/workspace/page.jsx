17b27e29069e314e8694e7a07a7961a5f4501ad0
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-09-01 14:52:15 +0200

    fix(projects): guard video workspace route

--- src/app/projects/video/workspace/page.jsx
+++ src/app/projects/video/workspace/page.jsx
@@ -1,7 +1,16 @@
 import AuthGate from '../../../../components/AuthGate';
 import MediaProjectWorkspace from '../../../../components/MediaProjectWorkspace';
+import ProjectWorkspaceGate from '../../../../components/ProjectWorkspaceGate';
 
-export default function VideoProjectPage({ searchParams }) {
-  const projectId = searchParams?.project || '';
-  return <AuthGate><MediaProjectWorkspace tool="video" projectId={projectId} /></AuthGate>;
+export default async function VideoProjectPage({ searchParams }) {
+  const params = await searchParams;
+  const projectId = params?.project || '';
+
+  return (
+    <AuthGate>
+      <ProjectWorkspaceGate tool="video" projectId={projectId}>
+        <MediaProjectWorkspace tool="video" projectId={projectId} />
+      </ProjectWorkspaceGate>
+    </AuthGate>
+  );
 }
