1f4e487568fbdb12e99fa6bd44b3ce857f18458a
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-09-01 14:51:57 +0200

    fix(projects): guard image workspace route

--- src/app/projects/images/workspace/page.jsx
+++ src/app/projects/images/workspace/page.jsx
@@ -1,12 +1,18 @@
 import { Suspense } from 'react';
 import AuthGate from '../../../../components/AuthGate';
 import ImageStudioWorkspace from '../../../../components/ImageStudioWorkspace';
+import ProjectWorkspaceGate from '../../../../components/ProjectWorkspaceGate';
 
-export default function ImageProjectWorkspacePage() {
+export default async function ImageProjectWorkspacePage({ searchParams }) {
+  const params = await searchParams;
+  const projectId = params?.project || '';
+
   return (
     <AuthGate>
       <Suspense fallback={<div className="min-h-[calc(100vh-5rem)] bg-[#050506]" />}>
-        <ImageStudioWorkspace />
+        <ProjectWorkspaceGate tool="images" projectId={projectId}>
+          <ImageStudioWorkspace />
+        </ProjectWorkspaceGate>
       </Suspense>
     </AuthGate>
   );
