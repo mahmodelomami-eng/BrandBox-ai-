ef1612b34feda2d2e30c0c5bf631131d4017bbb2
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-09-03 00:43:45 +0200

    feat(trends): expose Trend Intelligence agent in Trend Lab

--- src/app/admin/trends/page.jsx
+++ src/app/admin/trends/page.jsx
@@ -2,10 +2,12 @@
 
 import AuthGate from '../../../components/AuthGate';
 import AdminTrendLabPanel from '../../../components/AdminTrendLabPanel';
+import TrendAgentStatusCard from '../../../components/TrendAgentStatusCard';
 
 export default function AdminTrendLabPage() {
   return (
     <AuthGate>
+      <TrendAgentStatusCard />
       <AdminTrendLabPanel />
     </AuthGate>
   );
