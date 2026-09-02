de86bf8cd868c4d28d39ed7110bf4848d5243929
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-09-02 19:49:55 +0200

    feat(theme): wire theme provider into app shell

--- src/components/layout/AppNavigationWrapper.jsx
+++ src/components/layout/AppNavigationWrapper.jsx
@@ -2,14 +2,17 @@
 
 import React from 'react';
 import GlobalNavigation from '../GlobalNavigation';
+import ThemeToggle from '../ThemeToggle';
 import UserExperienceEnhancer from '../UserExperienceEnhancer';
+import { ThemeProvider } from '../../context/ThemeContext';
 
 export default function AppNavigationWrapper({ children }) {
   return (
-    <>
+    <ThemeProvider>
       <GlobalNavigation />
       <UserExperienceEnhancer />
-      <div className="pt-20">{children}</div>
-    </>
+      <div className="brandbox-theme-scope min-h-screen pt-20">{children}</div>
+      <ThemeToggle />
+    </ThemeProvider>
   );
 }
