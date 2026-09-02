a421326dc54b389a3b36d19dbc18daf125df3ad4
mahmodelomami-eng <mahmod.elomami@gmail.com>
2026-09-02 02:55:16 +0200

    fix(admin): mount global user presence heartbeat

--- src/components/layout/AppNavigationWrapper.jsx
+++ src/components/layout/AppNavigationWrapper.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import GlobalNavigation from '../GlobalNavigation';
+import UserExperienceEnhancer from '../UserExperienceEnhancer';
 
 export default function AppNavigationWrapper({ children }) {
   return (
     <>
       <GlobalNavigation />
+      <UserExperienceEnhancer />
       <div className="pt-20">{children}</div>
     </>
   );
